33f83b85181b2447bc328ec9ea17ef7468d6f6d7
vishallakhmapurkar <lakhmapurkar.vishal@gmail.com>
2025-12-09 01:38:55 +0530

    new ui

---
+++ dbt_project/models/staging/stg_orders.sql
@@ -0,0 +1,8 @@
+{{ config(materialized='view') }}
+
+SELECT
+    order_id AS order_id,
+    customer_id AS customer_id,
+    order_date AS order_date,
+    status AS status
+FROM {{ source('dbt_vlakhmapurkar', 'orders') }}
